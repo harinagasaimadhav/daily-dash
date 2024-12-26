5df62b215013a49a217f027e6f7407305a69afc5
harinagasai <hari.naga@probeinformation.com>
2024-12-26 20:59:01 +0530

    added login route for user.

--- src/backend/routes/userRoute.ts
+++ src/backend/routes/userRoute.ts
@@ -3,7 +3,15 @@ import { Router } from "jsr:@oak/oak";
 const router = new Router();
 
 router.post("/register", async (ctx) => {
+  ctx.response.status = 201;
   ctx.response.body = `user registered`;
+}).post("/login", async (ctx) => {
+  ctx.response.status = 200;
+  ctx.response.body = `user logged in`;
+  ctx.response.headers.set(
+    "Set-Cookie",
+    "cookie for authentication",
+  );
 });
 router.get("/", async (context) => {
   context.response.body = "user route";
