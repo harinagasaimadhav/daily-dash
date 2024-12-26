8c59bb8f4a16d2d0e92af8102e119a82d21bc529
harinagasai <hari.naga@probeinformation.com>
2024-12-26 20:45:52 +0530

    created user register route.

--- src/backend/routes/userRoute.ts
+++ src/backend/routes/userRoute.ts
@@ -2,6 +2,9 @@ import { Router } from "jsr:@oak/oak";
 
 const router = new Router();
 
+router.post("/register", async (ctx) => {
+  ctx.response.body = `user registered`;
+});
 router.get("/", async (context) => {
   context.response.body = "user route";
 });
