9c0cca0f0e1fcbe3b8790f010d31fd43a65cc9a7
harinagasai <hari.naga@probeinformation.com>
2024-12-27 22:22:41 +0530

    created UserController and UserService.

--- src/backend/routes/userRoute.ts
+++ src/backend/routes/userRoute.ts
@@ -1,26 +1,26 @@
 import { Router } from "jsr:@oak/oak";
+import { UserController } from "../controllers/UserController.ts";
 
 const router = new Router();
+const userController = new UserController();
 
-router.post("/register", async (ctx) => {
-  ctx.response.status = 201;
-  ctx.response.body = `user registered`;
-}).post("/login", async (ctx) => {
-  ctx.response.status = 200;
-  ctx.response.body = `user logged in`;
-  ctx.response.headers.set(
-    "Set-Cookie",
-    "cookie for authentication",
-  );
-}).get("/logout", async (ctx) => {
-  ctx.response.status = 200;
-  ctx.response.body = `user logged out`;
-}).delete("/", async (ctx) => {
-  const requestBody = await ctx.request.body.json();
+router.post("/register", userController.createUser)
+  .post("/login", async (ctx) => {
+    ctx.response.status = 200;
+    ctx.response.body = `user logged in`;
+    ctx.response.headers.set(
+      "Set-Cookie",
+      "cookie for authentication",
+    );
+  }).get("/logout", async (ctx) => {
+    ctx.response.status = 200;
+    ctx.response.body = `user logged out`;
+  }).delete("/", async (ctx) => {
+    const requestBody = await ctx.request.body.json();
 
-  ctx.response.status = 200;
-  ctx.response.body = `user with id ${requestBody.id} deleted successfully`;
-});
+    ctx.response.status = 200;
+    ctx.response.body = `user with id ${requestBody.id} deleted successfully`;
+  });
 router.get("/", async (context) => {
   context.response.body = "user route";
 });
